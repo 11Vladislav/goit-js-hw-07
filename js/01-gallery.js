b3e5ca4d4fd2a409ead47ed162b9d9d8a63ddfdf
Vladislav Ponomarev <vlad11ponomarev@gmail.com>
2022-03-27 19:58:54 +0300

    update hometask

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+galleryEl.addEventListener('click', clickPic);
+galleryCreate(galleryItems, galleryEl);
+
+function clickPic(event) {
+     if (event.target.nodeName !== 'IMG') {
+    return;
+     }
+     const frame = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+  frame.show();
+    window.addEventListener('keydown', event => (
+        event.code === 'Escape' ? frame.close() : event));
+}
+function galleryCreate(list, place) {
+  const elMarkup = list.map( event =>
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${event.original}">
+                <img
+                class="gallery__image"
+                src="${event.preview}"
+                data-source="${event.original}"
+                alt="${event.description}"
+                onclick='event.preventDefault()'
+                />
+            </a>
+        </div>`
+    )
+    .join('');
+
+  place.innerHTML = elMarkup;
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+galleryCreate(galleryItems, galleryEl);
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
+function galleryCreate(list, place) {
+    const elMarkup = list.map(event => `<a class="gallery__item" href="${event.original}" onclick="event.preventDefault()">
+        <img class="gallery__image" src="${event.preview}" alt="${event.description}" />
+        </a>`).join('')
+    place.innerHTML = elMarkup;
+}
